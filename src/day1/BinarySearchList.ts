e3b2efded756a7d262fef5a6122a5c277b150887
JMendioroz <jmendiorozredin@gmail.com>
2023-11-18 21:07:39 +0100

    COMPLETE BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor((low + high) / 2);
+        const val = haystack[mid];
+
+        if (val === needle) return true;
+        if (val < needle) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    } while (low < high);
+
+    return false;
 }
