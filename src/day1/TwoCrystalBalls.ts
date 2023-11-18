f74b634cfc4a349b92fb5361786ef3ee158f50ef
JMendioroz <jmendiorozredin@gmail.com>
2023-11-18 21:08:03 +0100

    COMPLETE TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jump = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jump;
+  for (; i < breaks.length; i += jump) {
+      if (breaks[i]) break;
+  }
+
+  for (let j = i - jump; j <= jump * i; j++) {
+      if (breaks[j]) return j;
+  }
+
+  return -1;
 }
